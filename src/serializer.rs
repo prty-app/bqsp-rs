bc5b4f6c1745726f903684a4e677dff2e81947a0
Dmitrij <49795742+DmitrijVC@users.noreply.github.com>
2023-04-02 14:08:17 +0200

    Add the Serializer trait

---
+++ src/serializer.rs
@@ -0,0 +1,62 @@
+use crate::{box_to_owned, BoxPack, Data};
+
+/// Used to serialize a specific structure into raw Data.
+pub trait Serializer {
+    type Error;
+
+    fn serialize(
+        &self,
+        data_type: impl Into<u16>,
+        queue: u8
+    ) -> Result<BoxPack<'_>, Self::Error>
+        where Self: Sized;
+
+    fn serialize_owned(
+        self,
+        data_type: impl Into<u16>,
+        queue: u8
+    ) -> Result<BoxPack<'static>, Self::Error>
+        where Self: Sized
+    {
+        let box_pack = self.serialize(
+            data_type,
+            queue
+        )?;
+
+        Ok(box_to_owned(box_pack))
+    }
+}
+
+
+// Vec<u8> can always be serialized into BoxPack
+impl Serializer for Vec<u8> {
+    type Error = std::convert::Infallible;
+
+    fn serialize(
+        &self,
+        data_type: impl Into<u16>,
+        queue: u8
+    ) -> Result<BoxPack<'_>, Self::Error>
+        where Self: Sized
+    {
+        Ok(BoxPack::new(
+            Data::from(self),
+            data_type,
+            queue
+        ))
+    }
+
+    fn serialize_owned(
+        self,
+        data_type: impl Into<u16>,
+        queue: u8
+    ) -> Result<BoxPack<'static>, Self::Error>
+        where Self: Sized
+    {
+        Ok(BoxPack::new(
+            Data::from(self),
+            data_type,
+            queue
+        ))
+    }
+}
